77c6f21929e4427826d11b90a869d9ccb8070c2c
dongY <ldcpaul@hanmail.net>
2019-01-03 05:34:22 +0900

    액션세그웨이 테스트

---
+++ ActionSegue/ActionSegue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActionSegue
+//
+//  Created by 이동영 on 03/01/2019.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
