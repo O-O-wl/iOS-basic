2c61cac11883ebe222957d7ccc6340c2772cc3e0
dongY <ldcpaul@hanmail.net>
2019-01-02 21:29:56 +0900

    2차 진행중

---
+++ IBTest/IBTest/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  IBTest
+//
+//  Created by 이동영 on 02/01/2019.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet var uiTitle01: UILabel!
+    @IBOutlet var uiTitle02: UILabel!
+    @IBOutlet var uiTitle03: UILabel!
+    @IBOutlet var uiTitle04: UILabel!
+    @IBOutlet var uiTitle05: UILabel!
+    
+    
+    @IBAction func clickBtn01(_ sender: Any) {
+    }
+    @IBAction func clickBtn02(_ sender: Any) {
+    }
+    
+    @IBAction func clickBtn03(_ sender: Any){}
+    @IBAction func clickBtn04(_ sender: Any){}
+    
+    @IBAction func clickBtn05(_ sender: Any) {
+    }
+}
+
