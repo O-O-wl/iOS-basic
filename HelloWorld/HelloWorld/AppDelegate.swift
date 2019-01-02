d89a649efde3c0721b3d2087903b1f10453cf25a
dongY <ldcpaul@hanmail.net>
2019-01-02 17:36:36 +0900

    AppDelegation 소스 분석

--- HelloWorld/HelloWorld/AppDelegate.swift
+++ HelloWorld/HelloWorld/AppDelegate.swift
@@ -8,15 +8,27 @@
 
 import UIKit
 
+
+//=========================================================
+//     UIApplication 에 AppDelegation이라고 알리는 에노테이션
+//=========================================================
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
+    /*******************************************
+            앱로딩되고 실행완료 직전에 실행되는 메소드
+     *******************************************/
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
         // Override point for customization after application launch.
+        
+        //==================
+        //  추가한 실행화면
+        // 보기 위해서 스레드 지연
+        //==================
         sleep(1)
         return true
     }
@@ -39,6 +51,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
+    /****************************************************
+    *
+    *       앱  종료 직전에 실행되는 메소드
+    *     데이터를 서버에 전송 저장 이런걸 적용하는 듯하다.
+    ******************************************************/
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
