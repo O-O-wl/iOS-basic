6cfb6f10a3fb5b0cb2a3d1e8ac66aeb4b9f7a690
dongY <ldcpaul@hanmail.net>
2019-01-05 23:00:08 +0900

    이미지피커 커트롤러 테스트

--- Delegate_Test/Delegate_Test/AppDelegate.swift
+++ Delegate_Test/Delegate_Test/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+gclass AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
--- Delegate_Test/Delegate_Test/ImagePickerControllerDelegationVC.swift
+++ Delegate_Test/Delegate_Test/ImagePickerControllerDelegationVC.swift
@@ -9,6 +9,33 @@
 import Foundation
 import UIKit
 
-class ImagePickerControllerDelegationVC : UIViewController{
+class ImagePickerControllerDelegationVC : UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
     
+    @IBOutlet var imgView: UIImageView!
+    @IBAction func btnPick(_ sender: UIButton) {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        picker.delegate = self
+        picker.present(picker, animated: false)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: false){ () in
+            let alert = UIAlertController(title: "이미지선택", message: "이미지 선택을 취소하셨습니다.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style:.cancel))
+            self.present(alert, animated: false )
+            
+            
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: false){
+            () in
+            let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            self.imgView.image = img
+        }
+    }
 }
