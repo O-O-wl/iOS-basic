9432a25e7346415748119790152f375991ba9ea9
dongY <ldcpaul@hanmail.net>
2019-01-10 21:45:33 +0900

    테이블뷰 init

---
+++ MyMovieChart/MyMovieChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by 이동영 on 10/01/2019.
+//  Copyright © 2019 이동영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
