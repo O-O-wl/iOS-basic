186ef1e5af5a1cc35e3aec46b325351b534846ec
dongY <ldcpaul@hanmail.net>
2019-01-15 02:06:46 +0900

    인디케이터 뷰 를 통한 로딩 구현

--- MyMovieChart/MyMovieChart/WebViewController.swift
+++ MyMovieChart/MyMovieChart/WebViewController.swift
@@ -13,14 +13,19 @@ class WebViewController : UIViewController{
     
     @IBOutlet var webView: WKWebView!
     
+    @IBOutlet var idcLoading: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.webView.navigationDelegate = self
+        self.webView.uiDelegate = self
+        
         if let url = UserDefaults.standard.url(forKey: "URL"){
             let req = URLRequest(url: url)
             //DispatchQueue.main.async {
             /// - Note: 웹뷰의 load 메소드는 네트워크상의 HTML문서요청하는 비동기 메소드
-                self.webView.load(req)
-           // }
+            self.webView.load(req)
+            // }
         }else {
             let alert = UIAlertController()
             alert.title = "오류!"
@@ -38,3 +43,42 @@ class WebViewController : UIViewController{
         
     }
 }
+// MARK: - WKNavigationDelegate 델리게이트 구현부
+extension WebViewController : WKNavigationDelegate{
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        self.idcLoading.startAnimating()
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        /// - Note:  인디케이터뷰의 behavior 속성이 'Hidden When Stopped' 이므로 이시점에서 Hidden 됨
+         self.idcLoading.stopAnimating()
+    }
+   // webview
+    
+}
+// MARK: - WKUIDelegate 델리게이트 구현부
+extension WebViewController : WKUIDelegate{
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        self.idcLoading.stopAnimating()
+        alert("상세페이지를 불러오지 못했습니다"){
+             _ = self.navigationController?.popViewController(animated: false)
+        }
+     }
+}
+// MARK: - Alert 부분 분리
+extension UIViewController {
+    func alert(_ message:String , onClick: (() -> Void)? = nil){
+  //  self.idcLoading.stopAnimating()
+    let alert =  UIAlertController(title: "네트워크오류", message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "확인", style: .cancel, handler:{ (_) in onClick?()})
+    alert.addAction(cancel)
+       // DispatchQueue.main.async {
+             self.present(alert, animated: false, completion: nil)
+       // }
+   
+}
+}
+
+
+
+
